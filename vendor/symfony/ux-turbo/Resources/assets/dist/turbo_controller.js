830f488a571ab65bde03d5924bd76ce22ef0ec54
Sina-glitch <137510596+Sina-glitch@users.noreply.github.com>
2023-08-28 15:42:22 +0200

    maj

--- vendor/symfony/ux-turbo/Resources/assets/dist/turbo_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from '@hotwired/stimulus';
-import * as Turbo from '@hotwired/turbo';
-
-window.Turbo = Turbo;
-class turbo_controller extends Controller {
-}
-
-export { turbo_controller as default };
